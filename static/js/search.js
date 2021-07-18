e4a0d3576a957cea69a847209e879a0a236ea1b2
Rohit Awate <rohitawate121@gmail.com>
2021-07-18 14:36:55 +0530

    Implement docked peptides searching

--- static/js/search.js
+++ static/js/search.js
@@ -10,19 +10,19 @@ QUERY SYNTAX:
 
 - A peptide can be queried using either one of its identifiers. An identifier is either a peptide's
   DRAMP ID or PEP ID.
-    Example #1: PEP1217
-    Example #2: DRAMP01472
+	Example #1: PEP1217
+	Example #2: DRAMP01472
 
 - Multiple peptides can be queried at once by separating their identifiers using commas.
-    Example: DRAMP01472, PEP1217
+	Example: DRAMP01472, PEP1217
 
 - A range of peptides can be queried by specifying the its PEP ID followed by the
   starting and ending (inclusive) index separated by a hyphen. Range queries are NOT
   allowed on DRAMP IDs since they are not sequential.
-    Example: PEP120-125
+	Example: PEP120-125
 
 - Multiple range queries can be made at a time by separating them with commas.
-    Example: PEP120-125
+	Example: PEP120-125
 
 NOTE: Only legal characters in query are the words "DRAMP", "PEP", 0-9, hyphens, commas and spaces.
 
@@ -35,13 +35,13 @@ QUERY ALGORITHM:
 - resultSet = []
 - for queryItem in queryItems
 	- Check if query item begins with DRAMP or PEP.
-    	- Stop processing query item if not.
+		- Stop processing query item if not.
 	- if queryItem starts with DRAMP:
 		- if queryItem contains hyphen
 			- Stop processing queryItem
 		- get PEP ID from selected[1] DRAMP_TO_PEP index
   
-  	- add PEP ID to resultSet
+		- add PEP ID to resultSet
 - Create cards based on PEP IDs in resultSet
 
 
@@ -61,6 +61,27 @@ const verifyRangeIndices = (indices) => {
 	return true;
 };
 
+const DOCKED_PEPS = new Set();
+
+const generateDockedPepsSet = () => {
+	const dockedPepsList = [
+		26, 49, 50, 51, 72, 88, 109, 113, 115, 167, 168, 187, 696, 782, 993,
+		994, 995, 996, 1005, 1086, 1087, 1088, 1089, 1218, 1219, 1245, 1326,
+		1437, 2442, 2533, 2534, 3052, 3059, 3219, 3239, 3240, 3292, 3296, 3297,
+		3298, 3308, 3309, 3349, 3366, 3461, 3469, 3470, 3475, 3983, 4020, 4021,
+		4583, 4674, 4707, 4708, 4709, 4710, 4785, 4819, 4844, 4886, 4931, 4932,
+		4933, 4934, 5235, 5236, 5238, 5240, 5241, 5242, 5243, 5244, 5245, 5363,
+		5481, 5482, 5483, 5484, 5485, 5486, 5487, 5488, 5489, 5490, 5491, 5492,
+		5493, 5494, 5495, 5496, 5497, 5498, 5499, 5500, 5501, 5502, 5505, 5508, 5509
+	];
+
+	for (const dockedPep of dockedPepsList) {
+		DOCKED_PEPS.add(dockedPep);
+	}
+};
+
+const isDocked = (pepID) => DOCKED_PEPS.has(pepID);
+
 const search = (query, env) => {
 	const queryItems = query.split(",").map((e) => e.trim());
 
@@ -73,6 +94,8 @@ const search = (query, env) => {
 		if (env === "ag+") {
 			const pepActivity = PEP_TO_ACTIVITY_AND_NAME[resultItem["pepID"]][0];
 			if (!(pepActivity === 1 || pepActivity === 3)) return;
+		} else if (env === "docked" && !isDocked(resultItem["pepID"])) {
+			return;
 		}
 
 		resultSet.push(resultItem);
@@ -156,3 +179,5 @@ const search = (query, env) => {
 		errors: errors,
 	};
 };
+
+generateDockedPepsSet();
