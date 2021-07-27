f43aec61ccc83bcf1fb1165f67da39a51c55948e
Rohit Awate <rohitawate121@gmail.com>
2021-07-27 23:00:31 +0530

    Just print out AG+, don't generate FASTA

--- scripts/agp_filter/agp_filter.py
+++ scripts/agp_filter/agp_filter.py
@@ -1,20 +1,12 @@
 from csv import DictReader
-from os import path, mkdir
 
 with open("../../utils/full.csv") as file:
     reader = DictReader(file)
 
-    if not path.isdir("AntiGramPositivePeptides"):
-        mkdir("AntiGramPositivePeptides")
-
     i = 2
     for row in reader:
         activity = row["Activity"]
-        if "Anti-Gram+" in activity:
-            with open(f"AntiGramPositivePeptides/Pep{i}.fasta", "w+") as fasta_file:
-                fasta_file.write(f">Peptide_{i}\n")
-                fasta_file.write(row["Sequence"])
-           		
+        if "Anti-Gram+" in activity:           		
                 print(f"Pep{i}")
 
         i = i + 1
