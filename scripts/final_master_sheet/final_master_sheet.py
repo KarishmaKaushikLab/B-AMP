6bfe3e353215d18bb1322a737d6d580bcfd3caca
Rohit Awate <rohitawate121@gmail.com>
2021-08-05 18:48:31 +0530

    CSV wrangling: Remove 18 peptides with unknown amino acids

--- scripts/final_master_sheet/final_master_sheet.py
+++ scripts/final_master_sheet/final_master_sheet.py
@@ -9,11 +9,33 @@ Final modifications to master sheet.
 
 import pandas as pd
 
-full_df = pd.read_csv("../../utils/full.csv")
+df = pd.read_csv("../../utils/full.csv")
 
 # Add a dedicated Pep ID column
-full_df.insert(0, "PepID", range(len(full_df)))
+df.insert(0, "PepID", range(len(df)))
 
+# Remove 18 peptides with unknown amino acids
+peps_to_remove = [
+    4913,
+    4914,
+    4915,
+    4916,
+    4917,
+    4918,
+    4919,
+    4922,
+    5503,
+    5504,
+    5506,
+    5507,
+    5532,
+    5533,
+    5534,
+    5535,
+    5536,
+    5537,
+]
+df = df[~df["PepID"].isin(peps_to_remove)]
 
 # Export out
-full_df.to_csv("final_sheet.csv", index=False)
+df.to_csv("final_sheet.csv", index=False)
